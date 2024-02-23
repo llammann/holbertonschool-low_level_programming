d2e15841a63466531c24355614b30282c50f656f
llammann <bd7n2v9tr@code.edu.az>
2024-02-23 23:35:50 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char ch = 'a';
+char cH = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (cH <= 'Z')
+{
+putchar(cH);
+cH++;
+return (0);
+}
