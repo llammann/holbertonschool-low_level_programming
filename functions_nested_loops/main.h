cee17131bc7e3b36c203906b7360d6815bf5cb4c
llammann <bd7n2v9tr@code.edu.az>
2024-02-24 22:09:27 +0000

    10xAlphabet

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype declaration for _putchar function */
 int _putchar(char c);
-
-/* Prototype declaration for print_alphabet function */
 void print_alphabet(void);
-
+void print_alphabet_x10(void)
 #endif /* MAIN_H */
