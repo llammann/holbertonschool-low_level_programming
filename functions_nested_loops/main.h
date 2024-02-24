0e5b1df5c2a4f32f113a98b2d6566d01112dfed3
llammann <bd7n2v9tr@code.edu.az>
2024-02-24 22:04:21 +0000

    fmain

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,10 @@
-int _putchar(char c)
-void print_alphabet()void
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype declaration for _putchar function */
+int _putchar(char c);
+
+/* Prototype declaration for print_alphabet function */
+void print_alphabet(void);
+
+#endif /* MAIN_H */
