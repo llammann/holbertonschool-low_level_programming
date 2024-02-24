f5aee9be3025f3aa3be8713d9b5ea89dd5f0342b
llammann <bd7n2v9tr@code.edu.az>
2024-02-24 20:46:47 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main-displaying _putchar
+ *Return:always 0
+ *Description:giving _putchar
+ **/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
