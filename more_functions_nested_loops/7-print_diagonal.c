6a1e635eefaf023849b5e9873c078f0d120cc6c0
llammann <bd7n2v9tr@code.edu.az>
2024-03-02 11:22:36 +0000

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+void print_diagonal(int n)
+{
+    int i, j;
+
+    if (n <= 0) {
+        _putchar('\n');
+        return;
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (j == i) {
+                _putchar('\\');
+	    } else {
+                _putchar(' ');  
+  	    }
+        }
+        _putchar('\n'); 
+    }
+}
+
