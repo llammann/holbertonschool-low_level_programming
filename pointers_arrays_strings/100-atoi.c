8a03b4ec786e90fd7aee5d71e35da73c3bf39c58
llammann <bd7n2v9tr@code.edu.az>
2024-03-02 16:23:52 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,40 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer
- * @s: The string to convert
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: The converted integer value
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-int sign = 1;
-int result = 0;
-int i = 0;
-while (s[i] == ' ')
-i++;
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
 if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
 {
-sign = -1;
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
 i++;
 }
-else if (s[i] == '+')
-{
-i++;
-}
-while (s[i] >= '0' && s[i] <= '9')
-{
-int digit = s[i] - '0';
-if (result > (INT_MAX - digit) / 10)
-{
-if (sign == 1)
-return (INT_MAX);
-else
-return (INT_MIN);
-}
-result = result * 10 + digit;
-i++;
-}
-return (sign *result);
+if (f == 0)
+return (0);
+return (n);
 }
