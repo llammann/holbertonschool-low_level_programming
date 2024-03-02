f392c5c4bdf101232e798e090ebfbc4d1293dd2b
llammann <bd7n2v9tr@code.edu.az>
2024-03-02 15:21:40 +0000

    each character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+
+i += 1;
+}
+putchar('\n');
+}
