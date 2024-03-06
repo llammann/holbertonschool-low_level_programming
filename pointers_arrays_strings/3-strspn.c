fe27012781e93150431c5d74de5a87b3c3564b1c
llammann <bd7n2v9tr@code.du.az>
2024-03-06 15:16:07 +0000

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to be searched
+ * @accept: the string containing the characters to match
+ *
+ * Return: return something
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+int i, j;
+int match = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+match++;
+}
+}
+return (match);
+}
