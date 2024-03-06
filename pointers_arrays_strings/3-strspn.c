47b4c85c831b1e9af0f948bf52fa8d8565c73bdc
llammann <bd7n2v9tr@code.du.az>
2024-03-06 15:25:22 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,6 +7,7 @@
  *
  * Return: return something
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 int i, j;
@@ -18,6 +19,12 @@ for (j = 0; accept[j] != '\0'; j++)
 if (s[i] == accept[j])
 {
 match++;
+break;
+}
+}
+if (accept[j] == '\0')
+{
+return (match);
 }
 }
 return (match);
