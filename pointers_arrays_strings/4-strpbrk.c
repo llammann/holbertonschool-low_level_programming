22a53964e2f66d7803cc7d4f705829e10c0d92a5
llammmann <bd7n2v9tr@code.edu.az>
2024-03-07 14:30:08 +0000

    new

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -12,10 +12,10 @@ char *_strpbrk(char *s, char *accept)
 int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept[j] != '\0' && s[i] != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
-return (s);
+return (s + i);
 }
 }
 return (NULL);
