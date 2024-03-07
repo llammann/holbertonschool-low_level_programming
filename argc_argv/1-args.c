a1022c7e525f9118d5c99a98d6b87cc538faf8d2
llammmann <bd7n2v9tr@code.edu.az>
2024-03-07 17:36:49 +0000

    new members

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: Array of pointers to strings containing the arguments
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
 return (0);
