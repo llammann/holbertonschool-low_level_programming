91f4462aa317703d766d547a9f40d5d54c7f53e1
llammmann <bd7n2v9tr@code.edu.az>
2024-03-07 17:31:00 +0000

    new members

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,11 +3,9 @@
 /**
  * main - Entry point of the program
  * @argc: Number of arguments passed to the program
- * @argv: Array of pointers to the arguments
- *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc)
 {
 printf("%d\n", argc - 1);
 return (0);
