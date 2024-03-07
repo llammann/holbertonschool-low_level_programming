137ed2f67fcec918d178d53c20abf889ee52c1ef
llammmann <bd7n2v9tr@code.edu.az>
2024-03-07 17:29:18 +0000

    new members

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
