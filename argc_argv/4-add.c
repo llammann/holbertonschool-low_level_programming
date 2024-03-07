1fd7b8aeeca68258aac0f28a5c5393034e0a5553
llammmann <bd7n2v9tr@code.edu.az>
2024-03-07 17:54:41 +0000

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,29 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: Number of arguments passed to the program
- * @argv: Array of pointers to strings containing the arguments
- * Return: 0 on success, 1 on error
- */
+*main - adds positive numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0 on success, 1 on failure
+*/
 
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc == 1)
-{
-printf("0\n");
-return (0);
-}
+int i, j, sum = 0;
 for (i = 1; i < argc; i++)
 {
-int num = atoi(argv[i]);
-if (num == 0 && argv[i][0] != '0')
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
 return (1);
 }
-sum += num;
+}
+sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 return (0);
