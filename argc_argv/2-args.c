3791771d39a593afce35e09670041ad9e55b70c2
llammmann <bd7n2v9tr@code.edu.az>
2024-03-07 17:38:37 +0000

    all argumnest

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to strings containing the arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
