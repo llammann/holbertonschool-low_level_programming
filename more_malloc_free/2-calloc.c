909b91cca77197a0a8100ef4b1cfc09bdbf25cd1
llammann <bd7n2v9tr@code.edu.az>
2024-03-12 14:28:22 +0000

    _calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,13 +12,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *ptr;
 unsigned int i;
 size_t total_size;
+char *char_ptr = ptr;
 if (nmemb == 0 || size == 0)
 return (NULL);
 total_size = nmemb * size;
 ptr = malloc(total_size);
 if (ptr == NULL)
 return (NULL);
-char *char_ptr = ptr;
 for (i = 0; i < total_size; i++)
 char_ptr[i] = 0;
 return (ptr);
