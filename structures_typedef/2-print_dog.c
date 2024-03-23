fae591f8ae1a4b58d6fd12af456fb85fa1caaff0
llammann <bd7n2v9tr@code.edu.az>
2024-03-23 20:26:06 +0000

    fck

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_dog - Prints the details of a struct dog
- * @d: Pointer to the struct dog variable to print
+ * print_dog -entry point
+ * @d: Entry of struct
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
-
-    printf("Name: %s\n", d->name ? d->name : "(nil)");
-    printf("Age: %.6f\n", d->age);
-    printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: %p\n", (void *)d->name);
+		else
+			printf("Name: %s\n",  d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: %p\n", (void *)d->owner);
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
-
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - Represents a dog with its name, age, and owner.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Description: This structure represents a dog
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
  */
-
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog - rename strucut with typedef
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */
