6c5ba5d1e5f21f45e84cb83988e441705118aa03
llammann <bd7n2v9tr@code.edu.az>
2024-03-23 19:13:46 +0000

    rf

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,11 +8,8 @@
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
-    printf("My name is %s, and I am %.2f :) - Woof!\n", my_dog.name, my_dog.age);
-
-    return (0);
+struct dog my_dog;
+init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+printf("My name is %s, and I am %.2f :) - Woof!\n", my_dog.name, my_dog.age);
+return (0);
 }
-
