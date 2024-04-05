ae8107c92749d41b781c6370cccc470fde9c70d1
llammann <bd7n2v9tr@cod.edu.az>
2024-04-05 14:53:14 +0000

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "lists.h"
 
-size_t print_list(const list_t *head)
+size_t print_list(const list_t *h)
 {
-    const list_t *current = head;
+    const list_t *current = h;
     size_t count = 0;
 
     while (current != NULL)
@@ -19,4 +19,3 @@ size_t print_list(const list_t *head)
 
     return count;
 }
-
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,13 @@
-#ifndef MAIN_h
-#define
+#ifndef LISTS_H
+#define LIST_H
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
-#endif MAIN_H
+#endif
