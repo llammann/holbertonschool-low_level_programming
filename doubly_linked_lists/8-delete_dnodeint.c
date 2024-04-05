92efc59ab07635265d3e878230cdbef251f16102
llammann <bd7n2v9tr@cod.edu.az>
2024-04-05 20:40:59 +0000

    remove node for index double list[A

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,6 +12,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 unsigned int i = 0;
+dlistint_t *current = *head;
 if (*head == NULL)
 return (-1);
 if (index == 0)
@@ -23,7 +24,6 @@ if (*head != NULL)
 (*head)->prev = NULL;
 return (1);
 }
-dlistint_t *current = *head;
 dlistint_t *prev = NULL;
 while (current != NULL && i < index)
 {
