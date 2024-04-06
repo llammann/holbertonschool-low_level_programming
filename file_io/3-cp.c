ea59a3757407b4d80d6855eac305e0b2333c6108
llammann <bd7n2v9tr@code.edu.az>
2024-04-06 21:31:47 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,8 +14,8 @@
 
 void handle_error(const char *msg, int code)
 {
-dprintf(STDERR_FILENO, "%s\n", msg);
-exit(code);
+    dprintf(STDERR_FILENO, "%s\n", msg);
+    exit(code);
 }
 
 /**
@@ -29,27 +29,24 @@ exit(code);
 
 int open_file(const char *filename, int flags, mode_t mode)
 {
-int fd = open(filename, flags, mode);
-if (fd == -1)
+    int fd = open(filename, flags, mode);
+    if (fd == -1)
+        handle_error("Error opening file", 98);
 
-handle_error("Error opening file", 98);
-
-return (fd);
+    return fd;
 }
 
 /**
  * Closes the file with the given file descriptor.
- * 
- * @param fd The file descriptor of the file to close
  */
 
 void close_file(int fd)
 {
-if (close(fd) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
+    if (close(fd) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+        exit(100);
+    }
 }
 
 /**
@@ -62,24 +59,28 @@ exit(100);
 
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to, bytes_read, bytes_written;
-char buffer[BUFFER_SIZE];
-if (argc != 3)
-handle_error("Usage: cp file_from file_to", 97);
+    int fd_from, fd_to, bytes_read, bytes_written;
+    char buffer[BUFFER_SIZE];
 
-fd_from = open_file(argv[1], O_RDONLY, 0);
-fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read)
-handle_error("Error writing to file", 99);
+    if (argc != 3)
+        handle_error("Usage: cp file_from file_to", 97);
 
+    fd_from = open_file(argv[1], O_RDONLY, 0);
+    fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written != bytes_read)
+            handle_error("Error writing to file", 99);
+    }
+
+    if (bytes_read == -1)
+        handle_error("Error reading from file", 98);
+
+    close_file(fd_from);
+    close_file(fd_to);
+    
+    return 0;
 }
-if (bytes_read == -1)
-handle_error("Error reading from file", 98);
 
-close_file(fd_from);
-close_file(fd_to);
-return (0);
-}
