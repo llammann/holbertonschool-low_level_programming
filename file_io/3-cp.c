99b1df8759f5d9abc85e54c9e87dc2406e38e6b2
llammann <bd7n2v9tr@code.edu.az>
2024-04-06 21:48:23 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>)
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -10,20 +10,21 @@ int main(int argc, char *argv[]) {
     const char *file_from = argv[1];
     const char *file_to = argv[2];
 
-    FILE *fptr1 = fopen(file_from, "r");
+    FILE *fptr1, *fptr2;
+    char buffer[1024];
+    size_t bytes_read;
+
+    fptr1 = fopen(file_from, "r");
     if (fptr1 == NULL) {
         dprintf(2, "Error: Can't read from file %s\n", file_from);
         exit(98);
     }
 
-    FILE *fptr2 = fopen(file_to, "w");
+    fptr2 = fopen(file_to, "w");
     if (fptr2 == NULL) {
         dprintf(2, "Error: Can't write to %s\n", file_to);
         exit(99);
     }
-
-    char buffer[1024];
-    size_t bytes_read;
 
     while ((bytes_read = fread(buffer, 1, sizeof(buffer), fptr1)) > 0) {
         size_t bytes_written = fwrite(buffer, 1, bytes_read, fptr2);
