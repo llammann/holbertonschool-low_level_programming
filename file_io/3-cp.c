58922000f297e922a9146dae7459c04e16701985
llammann <bd7n2v9tr@code.edu.az>
2024-04-06 21:53:00 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,43 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-        exit(97);
-    }
+int main() 
+{ 
+	FILE *fptr1, *fptr2; 
+	char filename[100], c; 
 
-    const char *file_from = argv[1];
-    const char *file_to = argv[2];
+	printf("Enter the filename to open for reading \n"); 
+	scanf("%s", filename); 
 
-    FILE *fptr1 = fopen(file_from, "r");
-    if (fptr1 == NULL) {
-        dprintf(2, "Error: Can't read from file %s\n", file_from);
-        exit(98);
-    }
+	// Open one file for reading 
+	fptr1 = fopen(filename, "r"); 
+	if (fptr1 == NULL) 
+	{ 
+		printf("Cannot open file %s \n", filename); 
+		exit(0); 
+	} 
 
-    FILE *fptr2 = fopen(file_to, "w");
-    if (fptr2 == NULL) {
-        dprintf(2, "Error: Can't write to %s\n", file_to);
-        exit(99);
-    }
+	printf("Enter the filename to open for writing \n"); 
+	scanf("%s", filename); 
 
-    char buffer[1024];
-    size_t bytes_read;
+	// Open another file for writing 
+	fptr2 = fopen(filename, "w"); 
+	if (fptr2 == NULL) 
+	{ 
+		printf("Cannot open file %s \n", filename); 
+		exit(0); 
+	} 
 
-    while ((bytes_read = fread(buffer, 1, sizeof(buffer), fptr1)) > 0) {
-        size_t bytes_written = fwrite(buffer, 1, bytes_read, fptr2);
-        if (bytes_written != bytes_read) {
-            dprintf(2, "Error: Can't write to %s\n", file_to);
-            exit(99);
-        }
-    }
+	// Read contents from file 
+	c = fgetc(fptr1); 
+	while (c != EOF) 
+	{ 
+		fputc(c, fptr2); 
+		c = fgetc(fptr1); 
+	} 
 
-    if (fclose(fptr1) != 0 || fclose(fptr2) != 0) {
-        dprintf(2, "Error: Can't close fd %d\n", fileno(fptr1));
-        exit(100);
-    }
+	printf("\nContents copied to %s", filename); 
 
-    return 0;
+	fclose(fptr1); 
+	fclose(fptr2); 
+	return 0; 
 }
 
